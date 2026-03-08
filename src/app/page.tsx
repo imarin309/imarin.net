d9f04046d446748288363271f9b86adffdf46379
imarin309 <16imarin309@gmail.com>
2026-03-08 21:29:43 +0900

    modify tag

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,6 +9,7 @@ export default function Home() {
 
   return (
     <div className="max-w-5xl mx-auto px-4 sm:px-6 lg:px-8 py-12">
+      <h1 className="sr-only">imarin.net - 記事一覧</h1>
       <PostList posts={sortedPosts} categories={categoryList} />
     </div>
   );
